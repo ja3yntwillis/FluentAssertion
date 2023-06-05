84e189613ad0990ea70a907f3ff31cdf000a8d31
jayan4537 <Jayanta.Dutta@towerswatson.com>
2023-06-05 12:44:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+string actual = "ABCDEFGHI";
+string expected = "ABCDEFGHI";
+ void normalAssertions(string text)
+{
+    try
+    {
+        Assert.AreEqual(actual, text+"df");
+    }
+    catch(Exception w)
+    {
+        Console.WriteLine(w.ToString());
+    }
+}
+
+ void usingFluentAssertions(string text)
+{
+    try
+    {
+        actual.Should().StartWith("AB").And.EndWith("HI").And.Contain("EFc").And.HaveLength(9).And.Be(expected);
+    }
+    catch (Exception ex) { Console.WriteLine( ex.Message); }
+}
+
+normalAssertions(expected);
+usingFluentAssertions(expected);
+
+
+
+
